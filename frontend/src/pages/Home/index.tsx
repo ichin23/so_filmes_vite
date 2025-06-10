a7e334fd8fb123cf88cbce038395c304e18526db
ichin23 <pedroferreiramanoel@gmail.com>
2025-06-10 09:00:40 -0300

    teste

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,17 +1,18 @@
 import { AvaliacaoCard } from "../../components/AvaliacaoCard";
 import { FilmeCard } from "../../components/FilmeCard";
+import { Header } from "../../components/Header";
 import { mockFilmes } from "../../mocks/FilmesMock";
 import { mockAvaliacoes } from "../../mocks/mockAvaliacoes";
 import { SHome } from "./styles"
 
 export function Home() {
     return <>
-        //TODO: Adicionar Header
+        <Header />
         <SHome>
             <h3>Mais Avaliados Essa semana</h3>
             <FilmeCard filmes={mockFilmes} />
             <h3>Comentários Recentes</h3>
-            <AvaliacaoCard avaliacoes={mockAvaliacoes} />
+            <AvaliacaoCard {...mockAvaliacoes} />
             <h3>Seus Favoritos</h3>
             <FilmeCard filmes={mockFilmes} />
         </SHome>
